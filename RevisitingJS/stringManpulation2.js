d7e761fb87c98454f7f90da2cec9a261a05b780e
Elizabeth Clawson <emclawson1@gmail.com>
2025-04-03 12:01:50 +0530

    added new folder and stringManipulation2.js

---
+++ RevisitingJS/stringManpulation2.js
@@ -0,0 +1,6 @@
+/*Lesson Overview
+
+Welcome! In this lesson, we'll delve into the basic string manipulation features of JavaScript, 
+which include string tokenization, string concatenation, trimming of whitespace from strings, 
+and type conversion operations. */
+
