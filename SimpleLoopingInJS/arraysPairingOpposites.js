d0218ed865c0517ffced6b4b598e1af89387fab9
Elizabeth Clawson <emclawson1@gmail.com>
2025-04-04 13:10:35 +0530

    added final problem for this unit

--- SimpleLoopingInJS/arraysPairingOpposites.js
+++ SimpleLoopingInJS/arraysPairingOpposites.js
@@ -395,3 +395,58 @@ return stringArray;
 }
 
 module.exports = { solution };
+
+//example: 
+
+/*
+You are provided with an array of n integers, where n ranges from 
+1 to 500 and is always an odd number. The elements of the array span values from 
+−10^6 to 10^6 inclusive. 
+The goal is to return a new array constructed by traversing the initial array in a specific order, 
+
+outlined as follows:
+
+- Begin with the middle element of the array.
+- For each subsequent pair of elements, alternate between taking two elements 
+from the left and two elements from the right, relative to the middle. 
+Take those two elements and place it to the back of the newly constructed array 
+preserving the order of the elements in the pair.
+
+- If fewer than two elements remain on either side, include all the remaining elements from that side.
+- Continue this process until all elements of the array have been traversed.
+
+For example, for array = [1, 2, 3, 4, 5, 6, 7], 
+your function should return [4, 2, 3, 5, 6, 1, 7]. 
+
+And for array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 
+your function should return [6, 4, 5, 7, 8, 2, 3, 9, 10, 1, 11].
+*/
+
+function unusualTraversal(array) {
+    // TODO: implement this function
+    let newArr = [];
+    let left, right; 
+    let length = array.length;
+    let mid = Math.floor(length/2);
+
+    
+
+    while (left >= 0 || right < length) {
+        if (left >= 1) {
+            newArr.push(array[left - 1], array[left]);
+            left -= 2;
+        } else if (left === 0) {
+            newArr.push(array[left]);
+            left--;
+        } if (right < length - 1) {
+            newArr.push(array[right], array[right + 1]);
+            right += 2;
+        } else if (right === length - 1) {
+        newArr.push(array[right]);
+        right++;
+        }
+    }
+return newArr;
+}
+
+module.exports = { unusualTraversal };
