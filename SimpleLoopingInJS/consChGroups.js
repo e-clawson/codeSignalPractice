33a0c21313437b442b8c61e2ede3717e7e5c0046
Elizabeth Clawson <emclawson1@gmail.com>
2025-04-05 14:54:39 +0530

    added another practice challenge

--- SimpleLoopingInJS/consChGroups.js
+++ SimpleLoopingInJS/consChGroups.js
@@ -149,3 +149,32 @@ for (let group of result) {
 
 //example: 
 
+/*
+In this task, you are to write a JavaScript function that implements the concept of 
+Run-Length Encoding (RLE) on an alphanumeric input string. 
+
+Run-length encoding is a simple form of data compression where 
+sequences of data entities that are the same are stored as a single data entity along with its count. 
+Each count must immediately follow the character it is associated with.
+
+Your function's name should be encodeRLE. 
+It takes a string as an input argument and returns 
+a new string that represents the input's run-length encoding.
+
+Your function should operate only on alphanumeric characters 
+(numbers 0-9 and uppercase and lowercase letters A-Z, a-z). 
+For any other types of characters in the string, 
+simply ignore them and do not include them in the final encoded output.
+
+For instance, if the input string is "aaabbcccdde", the output should be "a3b2c3d2e1". 
+If the input string includes non-alphanumeric characters, such as "aaa@@bb!!c#d**e", 
+the output should be "a3b2c1d1e1".
+
+We assume that the given input string could have up to 500 characters.
+*/
+
+function encodeRLE(s) {
+    // TODO: implement
+}
+
+module.exports = { solution: encodeRLE };
