af997006c0b4f6bd46f07b33be358b6974c8bfd1
Elizabeth Clawson <emclawson1@gmail.com>
2025-04-16 15:07:21 +0530

    added parsingAndManipulation.js and added notes

---
+++ StringOpsandTypeConv/parsingAndManipulation.js
@@ -0,0 +1,28 @@
+/*
+In this lesson, we will explore the practical applications of string operations and type conversions in JavaScript. 
+These concepts are crucial and are used in many programming areas. We'll examine a real-world example: time parsing. 
+Have you ever wondered how to add a certain number of seconds to a specific time? By the end of today's session, 
+you'll be equipped to calculate this using JavaScript. Let's get started!
+*/
+
+/*
+Task Statement and Description
+
+Imagine this: You receive a time formatted as a string in HH:MM:SS,
+where HH, MM, and SS denote the hour, minute, and second, respectively. 
+You are also given an integer representing a certain number of seconds. 
+Your task is to calculate the new time after adding the provided seconds, 
+then output the result in the HH:MM:SS format.
+
+For example, if the input time is 05:10:30 and the number of seconds to 
+add is 123, the output should be 05:12:33 since 123 seconds translate to 
+2 minutes and 3 seconds.
+
+Please note these points when resolving this task:
+
+The input time is always a valid time string in the HH:MM:SS format,
+with HH ranging from 00 to 23 and MM and SS ranging from 00 to 59.
+The output should be a time in the same format.
+Now let's go ahead and break down how to achieve this in our step-by-step 
+solution guide.
+*/
