e8837a8261c8aa65159b0f175ac11da76b75e394
Elizabeth Clawson <emclawson1@gmail.com>
2025-07-08 11:42:41 +0530

    added problem 3

--- js-int-practice/unit2/basic-array-ops.js
+++ js-int-practice/unit2/basic-array-ops.js
@@ -65,3 +65,73 @@ function findMin(arr) {
 
 // module.exports = { findMin }; 
 // got the whole for loop on my own, got help for the arr.length portion
+
+// problem 2: 
+
+//You are given an array of integers. 
+// Your job is to return the count of even and odd integers in the given array 
+// without using any built-in JavaScript methods. 
+//If the array is empty, the function should return { even_count: 0, odd_count: 0 }.
+
+//Your function should return an object in the format { even_count: x, odd_count: y }, 
+// where even_count represents the number of even integers and odd_count represents the number of odd integers in the provided array.
+
+function countEvenOdd(nums) {
+    // TODO: implement the function to return an object { even_count: x, odd_count: y }
+    let even_count = 0;
+    let odd_count = 0; 
+    
+    if (nums.length === 0){ 
+        return { even_count: 0, odd_count: 0 };
+    }
+    for (let i=0; i < nums.length; i++) {
+        let currentNum = nums[i]; 
+        
+        if (currentNum % 2 === 0) {
+            even_count++; 
+        } 
+        else if (currentNum % 2 !== 0) {
+            odd_count++; 
+        }
+    }
+    return { "even_count": even_count, "odd_count": odd_count}
+}
+
+// module.exports = { countEvenOdd };
+
+//problem 3: 
+//You are given an array of integers. 
+//Your task is to write a function in JavaScript that returns the number of times the smallest element appears in the array.
+
+//Please note that built-in methods such as Math.min() or Array.prototype.filter() should not be used in this task. 
+// Your goal is to accomplish this task by iterating over the array elements manually. 
+// Try to solve the task by doing just a single array traversal.
+
+function countMin(numbers) {
+    // TODO: Implement this function to count the smallest integer in the array.
+    
+    //can't use math.min 
+    //set lowest number, and set number count
+    //loop - search for lower number, and if lower, add +1 to number count 
+    let lowest_num = numbers[0]; 
+    let lowest_num_count = 1;
+    
+    if (numbers.length === 0){
+            return 0;
+    } 
+    
+    for (let i=1; i<numbers.length; i++){ 
+        let currentNum = numbers[i];
+        
+        if (currentNum < lowest_num) {
+            lowest_num = currentNum;
+            lowest_num_count = 1; 
+        } else if (currentNum === lowest_num) {
+            lowest_num_count ++; 
+        }
+    }
+    return lowest_num_count;
+    
+}
+
+//problem 4: 
