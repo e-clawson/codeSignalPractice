43f4ba556e338571fe906661ada0c06f88e388fa
Elizabeth Clawson <emclawson1@gmail.com>
2025-07-09 12:03:04 +0530

    solved problem 3

--- js-int-practice/Unit3/st-math-algos.js
+++ js-int-practice/Unit3/st-math-algos.js
@@ -65,4 +65,39 @@ function isPerfectSquare(n) {
 
 module.exports = { isPerfectSquare };
 
-//example 2 - find the next prime 
+//example 2 - find the next prime //You are given an integer number, 
+//1 ≤ n ≤ 10^6. Your task is to write a function nextPrime(n), 
+// that takes an integer n as input and returns the smallest prime number larger than n.
+
+//Here are some examples:
+
+// nextPrime(7) should return 11, because 11 is the next prime number after 7.
+// nextPrime(13) should return 17, because 17 is the next prime number after 13.
+// nextPrime(50) should return 53, because 53 is the next prime number after 50.
+
+function nextPrime(n) {
+    // TODO: implement the function to find the next prime number after n
+    let candidate = n + 1; 
+    
+    while (isPrime(candidate) === false) {
+        candidate++;
+    }
+    return candidate
+    
+}
+
+function isPrime(candidate) {
+    if (candidate <= 1) {
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt(candidate); i++) {
+        if (candidate % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+module.exports = { nextPrime };
+
+//problem 3 - 
